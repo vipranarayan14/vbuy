9dd6e718ac683d5a09ab48c2e4686661e4c7acdf
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-08-29 13:14:06 +0530

    include refs and ts in List
    refs are needed for deleting or updating items.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,19 +2,25 @@ import React from "react";
 
 import styles from "./List.module.css";
 
-export interface ItemType {
-  name: string;
-  bought: boolean;
-}
+export type ItemType = {
+  data: {
+    name: string;
+    bought: boolean;
+  };
+  ref: object;
+  ts: number;
+};
+
+export type ListType = Array<ItemType>;
 
 type ListProps = {
-  list: Array<ItemType>;
+  list: ListType;
   toggleBought: (id: number) => void;
 };
 
 type ListItemProps = {
   id: number;
-  data: ItemType;
+  data: ItemType["data"];
   toggleBought: ListProps["toggleBought"];
 };
 
@@ -33,7 +39,7 @@ const ListItem: React.FC<ListItemProps> = ({ id, data, toggleBought }) => (
 export const List: React.FC<ListProps> = ({ list, ...props }) => (
   <ul>
     {list.map((item, id) => (
-      <ListItem id={id} data={item} {...props} key={id} />
+      <ListItem id={id} data={item.data} {...props} key={id} />
     ))}
   </ul>
 );
