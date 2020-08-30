8a5b2b454690fb84d021f441387eb09d77bcaf51
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-08-30 19:01:36 +0530

    feat(api): add support for updating item

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,30 @@
 import { Item } from "../components/Item";
 
-export const requestAddItem = async (item: Item["data"]): Promise<Item> => {
+export const requestAddItem = async (data: Item["data"]): Promise<Item> => {
   const response = await fetch("/api/item-add", {
     method: "POST",
-    body: JSON.stringify(item),
+    body: JSON.stringify(data),
   });
 
   return response.json();
 };
 
-export const requestDeleteItem = async (item: Item): Promise<Boolean> => {
+export const requestDeleteItem = async (refId: number): Promise<Boolean> => {
   const response = await fetch("/api/item-delete", {
     method: "DELETE",
-    body: JSON.stringify(item),
+    body: JSON.stringify(refId),
   });
 
   return response.ok;
+};
+
+export const requestUpdateItem = async (refId: number, data: Item["data"]) => {
+  const response = await fetch("/api/item-update", {
+    method: "PATCH",
+    body: JSON.stringify({ refId, data }),
+  });
+
+  return response.json();
 };
 
 export const requestGetAllItems = async () => {
